54bb84fc7bd2733daeae744b3ab99e28f761d446
Roger Pedros <pedrooos.46@gmail.com>
2017-10-31 02:18:11 +0100

    Beta 1.7:
    -Neteja general

--- Background/Listener.js
+++ Background/Listener.js
@@ -1,15 +1,11 @@
 /*Copyright (C) 2017, Roger Pedrós Villorbina, All rights reserved.*/
 
 let configDataJsonUrl = "https://api.myjson.com/bins/13uvth";
-let servicesJsonUrl = "https://api.myjson.com/bins/m3rbx";
-let defauldBoardJsonUrl = "https://api.myjson.com/bins/bekth";
 
-( () => {
+(() => {
     /*Comprovacions inicials*/
     setContextMenu();
-
     //newUpdate();
-
 })();
 
 
@@ -19,20 +15,6 @@ chrome.runtime.onInstalled.addListener((details) => {
         newInstall()
             .then(() => {
                 getJSON(configDataJsonUrl)
-                    .then((textData) => {
-                        let jsonData = JSON.parse(textData);
-                        saveDatatoDB(jsonData)
-                    })
-            })
-            .then(() => {
-                getJSON(servicesJsonUrl)
-                    .then((textData) => {
-                        let jsonData = JSON.parse(textData);
-                        saveDatatoDB(jsonData)
-                    })
-            })
-            .then(() => {
-                getJSON(defauldBoardJsonUrl)
                     .then((textData) => {
                         let jsonData = JSON.parse(textData);
                         saveDatatoDB(jsonData)
@@ -51,10 +33,23 @@ chrome.runtime.onInstalled.addListener((details) => {
     }
 });
 
+let newInstall = () => {
+    return new Promise((resolve, reject) => {
+        //TODO Google Analytics event goes here
+        resolve()
+    });
+};
+let newUpdate = () =>{
+    return new Promise((resolve, reject) => {
+        //TODO Google Analytics event goes here
+        resolve()
+    });
+};
+
+
 let getJSON = (url) => {
     return new Promise((resolve, reject) => {
         var data = null;
-
         var xhr = new XMLHttpRequest();
         xhr.withCredentials = true;
 
@@ -71,39 +66,11 @@ let getJSON = (url) => {
     });
 };
 
-//TODO REORGANITZACIO PENDENT
-let newInstall = () => {
-    return new Promise((resolve, reject) => {
-        //TODO Google Analytics event goes here
-        resolve()
-    });
-};
-let newUpdate = () =>{
-    updateServices()
-        // .then(() => {
-        //     getJSON(servicesJsonUrl)
-        //         .then((textData) => {
-        //             let jsonData = JSON.parse(textData);
-        //             saveDatatoDB(jsonData)
-        //         })
-        // })
-        .then(() => {
-            setTimeout(() => {
-                chrome.runtime.openOptionsPage();
-            }, 1500);
-        });
-
-};
-let updateServices = () => {
-    return new Promise((resolve, reject) => {
-        //TODO Google Analytics event goes here
-        resolve()
-    });
-};
-//TODO REORGANITZACIO PENDENT
-
-/*Functions called form contexMenu onCick*/
+/*Functions called form contexMenu onCick
+* Generador del menu, quan el boto dret es clicat
+* */
 function getAndOpenPreSavedSession() {
+
     getPreSavedSession()
         .then((preSavedSessionFromDB) => {
             openSavedTabs(preSavedSessionFromDB.preSavedSession);
